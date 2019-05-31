77e24f6bc8de6216d63ae3cc4880dc8cdee6fbab
akninety <aekordas@gmail.com>
2019-05-30 23:32:59 -0400

    Add files via upload

---
+++ week1-019.AgeCheck/src/AgeCheck.java
@@ -0,0 +1,20 @@
+
+import java.util.Scanner;
+
+public class AgeCheck {
+
+    public static void main(String[] args) {
+        Scanner reader = new Scanner(System.in);
+
+        System.out.println("How old are you?");
+        int age = Integer.parseInt(reader.nextLine());
+
+        if (age < 0){
+            System.out.println("Impossible!");
+        } else if (age > 120){
+            System.out.println("Impossible!");
+        } else
+            System.out.println("OK");
+
+    }
+}
