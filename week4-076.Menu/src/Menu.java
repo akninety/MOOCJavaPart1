c4b6d73406a71d393b6c3745a732bbcdf6610eab
akninety <aekordas@gmail.com>
2019-06-08 19:26:34 -0400

    Add files via upload

---
+++ week4-076.Menu/src/Menu.java
@@ -0,0 +1,30 @@
+
+import java.util.ArrayList;
+
+public class Menu {
+
+    private ArrayList<String> meals;
+
+    public Menu() {
+
+        this.meals = new ArrayList<String>();
+
+    }
+
+    public void addMeal(String meal){
+            if(!this.meals.contains(meal)){
+                this.meals.add(meal);
+            }
+
+    }
+
+    public void printMeals(){
+        for(String item : meals){
+            System.out.println(this.meals);
+        }
+    }
+
+    public void clearMenu(){
+        meals.clear();
+    }
+}
