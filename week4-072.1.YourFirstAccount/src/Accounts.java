fa413becc96c8eb0f7071e9ceaba299695ef8892
akninety <aekordas@gmail.com>
2019-06-07 23:22:17 -0400

    Add files via upload

---
+++ week4-072.1.YourFirstAccount/src/Accounts.java
@@ -0,0 +1,19 @@
+
+import java.util.Scanner;
+
+public class Accounts {
+
+    public static void main(String[] args) {
+        // Code in Account.Java should not be touched!
+        // write your code here
+
+        Account alexAccount = new Account("Alex's account",100);
+
+        alexAccount.deposit(20);
+
+        System.out.println(alexAccount);
+
+
+    }
+
+}
